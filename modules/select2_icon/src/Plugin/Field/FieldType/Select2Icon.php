56c343ac565a14adb54fec0cfb0c42bf05e9074d
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-03-12 09:07:04 +0200

    Fixed Select2 icon field type, formatter and widget.

--- modules/select2_icon/src/Plugin/Field/FieldType/Select2Icon.php
+++ modules/select2_icon/src/Plugin/Field/FieldType/Select2Icon.php
@@ -3,8 +3,8 @@
 namespace Drupal\select2_icon\Plugin\Field\FieldType;
 
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
-use Drupal\Core\Field\Plugin\Field\FieldType\StringItemBase;
 use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\Field\FieldItemBase;
 
 /**
  * Plugin implementation of the 'select2_icon' field type.
@@ -12,29 +12,21 @@ use Drupal\Core\TypedData\DataDefinition;
  * @FieldType(
  *   id = "select2_icon",
  *   label = @Translation("Select2 Icon"),
+ *   category = @Translation("Helfi"),
  *   default_widget = "select2_icon_widget",
+ *   default_formatter = "select2_icon_formatter"
  * )
  */
-class Select2Icon extends StringItemBase {
+class Select2Icon extends FieldItemBase {
+
+  const SELECT2_ICON_CACHE = 'select2_icon_cache';
 
   /**
    * {@inheritdoc}
    */
-  public static function defaultFieldSettings() {
-    return [
-      'title' => DRUPAL_OPTIONAL,
-    ] + parent::defaultFieldSettings();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
-    $properties['value'] = DataDefinition::create('string')
-      ->setLabel(t('Icon ID'))
-      ->addConstraint('Length', ['max' => 127])
-      ->setRequired(TRUE);
-
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $storage) {
+    $properties = [];
+    $properties['icon'] = DataDefinition::create('string')->setLabel(t('Icon ID'));
     return $properties;
   }
 
@@ -44,14 +36,10 @@ class Select2Icon extends StringItemBase {
   public static function schema(FieldStorageDefinitionInterface $field_definition) {
     return [
       'columns' => [
-        'value' => [
-          'description' => 'Icon ID.',
-          'type' => 'varchar',
-          'length' => 127,
+        'icon' => [
+          'type' => 'char',
+          'length' => 255,
         ],
-      ],
-      'indexes' => [
-        'value' => [['value', 20]],
       ],
     ];
   }
@@ -59,8 +47,41 @@ class Select2Icon extends StringItemBase {
   /**
    * {@inheritdoc}
    */
-  public static function mainPropertyName() {
-    return 'value';
+  public function isEmpty() {
+    return empty($this->get('icon')->getValue());
+  }
+
+  /**
+   * Load icons array.
+   *
+   * Load icons either from cache or load them based on the data received from
+   * json-file which is saved in configuration.
+   *
+   * @return array|false
+   *   Returns and array of icons or false.
+   */
+  public static function loadIcons() {
+    if ($icons = \Drupal::cache()->get(static::SELECT2_ICON_CACHE)) {
+      return $icons->data;
+    }
+    else {
+      $config = \Drupal::getContainer()->get('config.factory')->getEditable('select2_icon.settings');
+      $json_path = \Drupal::root() . $config->get('path_to_json');
+
+      $data = file_get_contents($json_path);
+
+      if ($data !== FALSE) {
+        $json = json_decode($data, TRUE);
+
+        if (is_array($json) && !empty($json)) {
+          $icons = array_combine($json, $json);
+
+          \Drupal::cache()->set(static::SELECT2_ICON_CACHE, $icons);
+          return $icons;
+        }
+      }
+    }
+    return FALSE;
   }
 
 }
--- modules/select2_icon/src/Plugin/Field/FieldWidget/Select2IconWidget.php
+++ modules/select2_icon/src/Plugin/Field/FieldWidget/Select2IconWidget.php
@@ -4,11 +4,10 @@ namespace Drupal\select2_icon\Plugin\Field\FieldWidget;
 
 use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\select2\Plugin\Field\FieldWidget\Select2Widget;
-use GuzzleHttp\Exception\RequestException;
-
-define('SELECT2_ICON_CACHE', 'select2_icon_cache');
+use Drupal\Core\Form\OptGroup;
+use Drupal\select2_icon\Plugin\Field\FieldType\Select2Icon;
 
 /**
  * Plugin implementation of the 'select2_icon_widget' widget.
@@ -21,23 +20,30 @@ define('SELECT2_ICON_CACHE', 'select2_icon_cache');
  *   }
  * )
  */
-class Select2IconWidget extends Select2Widget {
+class Select2IconWidget extends WidgetBase {
 
   /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-    $element = parent::formElement($items, $delta, $element, $form, $form_state);
-
     global $base_secure_url;
     $config = \Drupal::getContainer()->get('config.factory')->getEditable('select2_icon.settings');
     $icon_path = $base_secure_url . '/' . $config->get('path_to_sprite');
 
-    $element['#theme'] = 'select2_icon';
-    $element['#icons_path'] = $icon_path;
-    $element['#attached']['library'][] = 'select2_icon/select2_icon';
-    $element['#attached']['drupalSettings']['select2Icon']['pathToIcons'] = $icon_path;
-    $element['#default_value'] = $this->getSelectedOptions($items);
+    $element['icon'] = [
+      '#type' => 'select2',
+      '#cardinality' => $this->fieldDefinition->getFieldStorageDefinition()->getCardinality(),
+      '#select2' => [
+        'width' => $this->getSetting('width') ?? '400px',
+      ],
+      '#theme' => 'select2_icon_widget',
+      '#icons_path' => $icon_path,
+      '#options' => Select2Icon::loadIcons(),
+      '#default_value' => $this->getSelectedOptions($items),
+    ];
+
+    $element['icon']['#attached']['library'][] = 'select2_icon/select2_icon';
+    $element['icon']['#attached']['drupalSettings']['select2Icon']['pathToIcons'] = $icon_path;
 
     return $element;
   }
@@ -53,41 +59,32 @@ class Select2IconWidget extends Select2Widget {
    */
   protected function getOptions(FieldableEntityInterface $entity) {
     if (!isset($this->options)) {
-      $this->options = $this->loadIcons();
+      $this->options = Select2Icon::loadIcons();
     }
     return $this->options;
   }
 
   /**
-   * Get icons either from cache or load them based on the data received from
-   * json-file which is saved in configuration.
+   * Determines selected options from the incoming field values.
    *
-   * @return array|false
-   *   Returns and array of icons or false.
+   * @param \Drupal\Core\Field\FieldItemListInterface $items
+   *   The field values.
+   *
+   * @return array
+   *   The array of corresponding selected options.
    */
-  private function loadIcons() {
-    if ($icons = \Drupal::cache()->get(SELECT2_ICON_CACHE)) {
-      return $icons->data;
-    }
-    else {
-      $config = \Drupal::getContainer()->get('config.factory')->getEditable('select2_icon.settings');
-      $json_path = \Drupal::root() . $config->get('path_to_json');
+  protected function getSelectedOptions(FieldItemListInterface $items) {
+    // In case this widget is used for opt groups.
+    $flat_options = OptGroup::flattenOptions($this->getOptions($items->getEntity()));
 
-      try {
-        $data = file_get_contents($json_path);
-        $json = json_decode($data, true);
-
-        if (is_array($json) && !empty($json)) {
-          $icons = array_combine($json, $json);
-
-          \Drupal::cache()->set(SELECT2_ICON_CACHE, $icons);
-          return $icons;
-        }
-      } catch (RequestException $e) {
-        watchdog_exception('select2_icon', $e);
+    $selected_options = [];
+    foreach ($items as $item) {
+      $value = $item->icon;
+      if (isset($flat_options[$value])) {
+        $selected_options[] = $value;
       }
     }
-    return FALSE;
+    return $selected_options;
   }
 
 }

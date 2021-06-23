cdb48f1ea87d1581620d36cd36ede0fd88c39bfc
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-06-23 11:14:26 +0300

    UHF-1747: Updated Webpack (4 --> 5) and updated dependencies accordingly.

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,58 @@
-const isDev = (process.env.NODE_ENV !== "production");
+const isDev = (process.env.NODE_ENV !== 'production');
 
-const path = require("path");
-const globImporter = require("node-sass-glob-importer");
+const path = require('path');
+const globImporter = require('node-sass-glob-importer');
 
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('@nuxt/friendly-errors-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 
 module.exports = {
   entry: {
-    styles: ["./src/scss/styles.scss"],
-    bundle: ["./src/js/common.js"],
-    languageSwitcher: ["./src/js/languageSwitcher"],
-    heroToggle: ["./src/js/heroToggle.js"],
-    columnsToggle: ["./src/js/columnsToggle.js"],
-    listOfLinks: ["./src/js/listOfLinks.js"],
-    tprEditForm: ["./src/js/tprEditForm.js"],
+    styles: ['./src/scss/styles.scss'],
+    bundle: ['./src/js/common.js'],
+    languageSwitcher: ['./src/js/languageSwitcher'],
+    heroToggle: ['./src/js/heroToggle.js'],
+    columnsToggle: ['./src/js/columnsToggle.js'],
+    listOfLinks: ['./src/js/listOfLinks.js'],
+    tprEditForm: ['./src/js/tprEditForm.js'],
+  },
+  experiments: {
+    asset: true
   },
   output: {
-    devtoolLineToLine: true,
-    path: path.resolve(__dirname, "dist"),
-    chunkFilename: "js/async/[name].chunk.js",
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: 'js/async/[name].chunk.js',
     pathinfo: true,
-    filename: "js/[name].min.js",
-    publicPath: "../",
+    filename: 'js/[name].min.js',
+    publicPath: '../',
   },
   module: {
-    rules: [{
-      test: /\.(config.js)$/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            name: "[path][name].[ext]",
-            outputPath: "./"
-          }
-        }
-      ]
-    },
+    rules: [
       {
         test: /\.(png|jpe?g|gif)$/,
-        use: [{
-          loader: "file-loader",
-          options: {
-            name: "media/[name].[ext]?[hash]",
-          },
+        generator: {
+          filename: 'media/[hash][ext]'
         },
-        ],
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|ttf|eot|svg)$/,
         include: [
-          path.resolve(__dirname, "src/fonts")
+          path.resolve(__dirname, 'src/fonts')
         ],
-        use: [{
-          loader: "file-loader",
-          options: {
-            name: "fonts/[name].[ext]",
-          },
+        generator: {
+          filename: 'fonts/[name][ext]'
         },
-        ],
-      },
-      {
-        test: /modernizrrc\.js$/,
-        loader: "expose-loader?Modernizr!webpack-modernizr-loader",
+        type: 'asset/resource',
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: ['babel-loader'],
+        type: 'javascript/auto',
       },
       {
         test: /\.(css|sass|scss)$/,
@@ -80,27 +60,27 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              name: "[name].[ext]?[hash]",
-            }
+              publicPath: path.resolve(__dirname, 'dist'),
+            },
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               sourceMap: isDev,
               importLoaders: 2,
             },
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
-              "postcssOptions": {
-                "config": path.join(__dirname, "postcss.config.js"),
+              'postcssOptions': {
+                'config': path.join(__dirname, 'postcss.config.js'),
               },
               sourceMap: isDev,
             },
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sourceMap: isDev,
               sassOptions: {
@@ -109,41 +89,42 @@ module.exports = {
             },
           },
         ],
+        type: 'javascript/auto',
       },
     ],
   },
   resolve: {
     modules: [
-      path.join(__dirname, "node_modules"),
+      path.join(__dirname, 'node_modules'),
     ],
-    extensions: [".js", ".json"],
+    extensions: ['.js', '.json'],
   },
   plugins: [
     new FriendlyErrorsWebpackPlugin(),
     new FixStyleOnlyEntriesPlugin(),
-    new CleanWebpackPlugin(["dist"], {
+    new CleanWebpackPlugin(['dist'], {
       root: path.resolve(__dirname),
     }),
     new CopyPlugin({
-      "patterns": [
+      'patterns': [
         {
-          "context": "./",
-          "from": "node_modules/select2/dist/js/select2.min.js",
-          "to": path.resolve(__dirname, "dist") + "/js/",
-          "force": true,
-          "flatten": true
+          'context': './',
+          'from': 'node_modules/select2/dist/js/select2.min.js',
+          'to': path.resolve(__dirname, 'dist') + '/js/',
+          'force': true,
+          'flatten': true
         },
         {
-          "context": "./",
-          "from": "node_modules/select2/dist/css/select2.min.css",
-          "to": path.resolve(__dirname, "dist") + "/css/",
-          "force": true,
-          "flatten": true
+          'context': './',
+          'from': 'node_modules/select2/dist/css/select2.min.css',
+          'to': path.resolve(__dirname, 'dist') + '/css/',
+          'force': true,
+          'flatten': true
         }
       ]
     }),
     new MiniCssExtractPlugin({
-      filename: "css/[name].min.css",
+      filename: 'css/[name].min.css',
     }),
   ],
   watchOptions: {

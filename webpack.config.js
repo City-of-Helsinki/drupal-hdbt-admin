0eea4a30a0f34b758a940389b74297db3918df7c
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-11-25 10:19:31 +0200

    Marked fsevents as optional and fixed CSS imports.

--- webpack.config.js
+++ webpack.config.js
@@ -65,6 +65,7 @@ module.exports = {
             options: {
               sourceMap: isDev,
               importLoaders: 2,
+              esModule: false,
             },
           },
           {

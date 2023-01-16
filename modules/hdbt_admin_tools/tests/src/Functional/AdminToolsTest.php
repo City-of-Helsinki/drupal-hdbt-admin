72d8ca50c9c70f8086c4f81628cc1814c143eb90
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2023-01-16 17:03:17 +0200

    UHF-7883: Added simple test for the HDBT Admin tools.

---
+++ modules/hdbt_admin_tools/tests/src/Functional/AdminToolsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\hdbt_admin_tools\Functional;
+
+use Drupal\Core\Url;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests HDBT admin tools module.
+ */
+class AdminToolsTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'hdbt_admin_tools',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Test HDBT Admin tools routes.
+   */
+  public function testAdminToolsRoutes() : void {
+    $routes = [
+      'hdbt_admin_tools.list_all' => 'Tools',
+      'hdbt_admin_tools.site_settings_form' => 'Site settings',
+      'hdbt_admin_tools.taxonomy' => '',
+    ];
+
+    foreach ($routes as $route => $title) {
+      $this->drupalGet(Url::fromRoute($route));
+      $this->assertSession()->statusCodeEquals(403);
+    }
+
+    $this->drupalLogin($this->createUser([
+      'access administration pages',
+      'administer nodes',
+      'access taxonomy overview'
+    ]));
+
+    foreach ($routes as $route => $title) {
+      $this->drupalGet(Url::fromRoute($route));
+      $this->assertSession()->statusCodeEquals(200);
+      if (!empty($title)) {
+        $this->assertSession()->pageTextContains($title);
+      }
+    }
+  }
+
+}

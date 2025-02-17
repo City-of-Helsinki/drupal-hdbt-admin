6968921c91e980c1203e425f2ca8bb75efa3d72a
Toni Sinisalo <toni.sinisalo@digitalist.com>
2025-02-17 13:32:58 +0200

    UHF-11449: UHF-11449: Fixing a missing 'Published'-checkbox on TPR entity forms.

--- src/js/tprEditForm.js
+++ src/js/tprEditForm.js
@@ -4,26 +4,24 @@
   Drupal.behaviors.tprEditForm = {
     attach: function attach() {
       const newParent = document.querySelector('.region-sticky__items__inner');
+      const statusInput = newParent.querySelectorAll('.js-tpr-entity-status');
 
-      if (newParent.querySelectorAll('.tpr-entity-form-actions').length > 0) {
-
+      if (statusInput.length > 0) {
         // Make content translation published status reactive.
         // eslint max-nested-callbacks
-        document
-          .querySelectorAll('.form-item--status [name="status"]')
-          .forEach((publishedState) => {
-            publishedState.addEventListener('click', (event) => {
-              const value = event.target.checked;
-              // Sync published state value.
-              document
-                .querySelectorAll(
-                  '.form-item--content-translation-status [name="content_translation[status]"]'
-                )
-                .forEach((publishedState) => {
-                  publishedState.checked = value;
-                });
-            });
+        statusInput.forEach((publishedState) => {
+          publishedState.addEventListener('click', (event) => {
+            const value = event.target.checked;
+            // Sync published state value.
+            document
+              .querySelectorAll(
+                '.form-item--content-translation-status [name="content_translation[status]"]'
+              )
+              .forEach((publishedState) => {
+                publishedState.checked = value;
+              });
           });
+        });
       }
     },
   };

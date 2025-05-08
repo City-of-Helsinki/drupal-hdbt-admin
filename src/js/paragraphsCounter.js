f348ce3c1f6f041039ad9abd8f4d17e761be1bb5
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-05-08 10:11:27 +0300

    UHF-11183: Simplified code structure and moved the variables outside drupal wrapper.

--- src/js/paragraphsCounter.js
+++ src/js/paragraphsCounter.js
@@ -1,55 +1,54 @@
 'use strict';
 
+// Global variables for paragraphs limit, delay and observer status.
+const paragraphsLimit = 40;
+const delay = 2000;
+let observerInitialized = false;
+
+/**
+ * Check how many paragraph elements exist on the page and updates
+ * the warning text accordingly. The functionality shows the warning text
+ * if there are more than 40 paragraphs on the page.
+ */
+const updateParagraphCounter = () => {
+  const counterWarning = document.querySelector('.paragraphs-counter__warning');
+  if (!counterWarning) return;
+
+  const allParagraphs = document.querySelectorAll('.paragraph-top:not([data-drupal-selector*="hero"])');
+
+  if (allParagraphs.length >= paragraphsLimit) {
+    const warningText = counterWarning.querySelector('.paragraphs-counter__warning-text');
+
+    counterWarning.classList.remove('is-hidden');
+    warningText.innerHTML = Drupal.t(
+      'The page contains many content elements (e.g., accordions, embeds, columns, banners): !counted. Consider reducing the number of elements to !total or moving some of the content to its own page. Having too many elements can prevent the page from being saved.',
+      {
+        '!counted': '<strong>' + allParagraphs.length + '</strong>',
+        '!total': '<strong>' + paragraphsLimit + '</strong>'
+      },
+      { context: 'Paragraphs counter' }
+    );
+  } else {
+    counterWarning.classList.add('is-hidden');
+  }
+};
+
+/**
+ * Avoid triggering expensive operations repeatedly.
+ *
+ * @param {Function} func - The function to debounce
+ * @param {number} wait - The delay in milliseconds
+ * @return {Function} - A debounced version of the input function
+ */
+const debounce = (func, wait = delay) => {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, wait);
+  };
+};
+
 ((Drupal) => {
-
-  // Global variables for paragraphs limit, delay and observer status.
-  const paragraphsLimit = 40;
-  const delay = 2000;
-  let observerInitialized = false;
-
-  /**
-   * Check how many paragraph elements exist on the page and updates
-   * the warning text accordingly. The functionality shows the warning text
-   * if there are more than 40 paragraphs on the page.
-   */
-  const updateParagraphCounter = () => {
-    const counterWarning = document.querySelector('.paragraphs-counter__warning');
-    if (!counterWarning) return;
-
-    const allParagraphs = document.querySelectorAll('.paragraph-top:not([data-drupal-selector*="hero"])');
-
-    if (allParagraphs.length >= paragraphsLimit) {
-      const warningText = counterWarning.querySelector('.paragraphs-counter__warning-text');
-
-      counterWarning.classList.remove('is-hidden');
-      warningText.innerHTML = Drupal.t(
-        'The page contains many content elements (e.g., accordions, embeds, columns, banners): !counted. Consider reducing the number of elements to !total or moving some of the content to its own page. Having too many elements can prevent the page from being saved.',
-        {
-          '!counted': '<strong>' + allParagraphs.length + '</strong>',
-          '!total': '<strong>' + paragraphsLimit + '</strong>'
-        },
-        { context: 'Paragraphs counter' }
-      );
-    } else {
-      counterWarning.classList.add('is-hidden');
-    }
-  };
-
-  /**
-   * Avoid triggering expensive operations repeatedly.
-   *
-   * @param {Function} func - The function to debounce
-   * @param {number} wait - The delay in milliseconds
-   * @return {Function} - A debounced version of the input function
-   */
-  const debounce = (func, wait = delay) => {
-    let timeout;
-    return function () {
-      clearTimeout(timeout);
-      timeout = setTimeout(func, wait);
-    };
-  };
-
   Drupal.behaviors.paragraphsCounter = {
     attach() {
       updateParagraphCounter();

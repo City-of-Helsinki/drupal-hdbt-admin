335b01c7129c040a20ea19657701c6d5567081d1
Anna Lonka <anna.lonka@druid.fi>
2025-10-10 14:39:27 +0300

    UHF-12244: Added warning message that hero and sidenavigation cannot be used together

--- src/js/heroToggle.js
+++ src/js/heroToggle.js
@@ -1,34 +1,78 @@
+'use strict';
+
 /**
  * Functionality that adds a hero to entity if the hero-checkbox is checked.
+ * Also adds a warning message that hero and side navigation cannot be used together.
  */
 
-// Boolean checkbox field that determines if there is hero added or not.
-const heroCheckbox = document.querySelector(
-  '.form-item--field-has-hero-value input.form-checkbox'
-);
-
-// Helper function to trigger event
-function triggerEvent(element, type) {
-  const event = new Event(type);
-  element.dispatchEvent(event);
-}
-
-if (heroCheckbox) {
-  heroCheckbox.addEventListener('click', function () {
-    if (this.checked) {
-      let addHero = document.querySelector(
-        '[data-hdbt-selector="field-hero-action--add"]'
-      );
-      if (addHero) {
-        triggerEvent(addHero, 'mousedown');
+(Drupal => {
+  Drupal.behaviors.heroToggle = {
+    attach(context) {
+      if (context !== document) {
+        return;
       }
-    } else if (!this.checked) {
-      let removeHero = document.querySelector(
-        '[data-hdbt-selector="field-hero-action--remove"]'
+
+      // Boolean checkbox field that determines if there is hero added or not.
+      const heroCheckbox = document.querySelector(
+        '.form-item--field-has-hero-value input.form-checkbox'
       );
-      if (removeHero) {
-        triggerEvent(removeHero, 'mousedown');
+
+      // Helper function to trigger event
+      function triggerEvent(element, type) {
+        const event = new Event(type);
+        element.dispatchEvent(event);
+      }
+
+      const isBasicPage = document.querySelector('[data-drupal-selector="node-page-edit-form"]')  ? true : false ;
+      const warningNotification = document.createElement('div');
+      warningNotification.className = 'form-notification form-notification--warning';
+
+      const iconContainer = document.createElement('div');
+      iconContainer.className = 'form-notification__icon';
+
+      const icon = document.createElement('span');
+      icon.className = 'hel-icon hel-icon--alert-circle-fill';
+      icon.role = 'img';
+
+      iconContainer.appendChild(icon);
+
+      const warningContainer = document.createElement('p');
+      warningContainer.className = 'form-notification__warning-text';
+      warningContainer.innerHTML = Drupal.t('The hero component and the side navigation cannot be used together. The side navigation will not be visible if the hero component is enabled.');
+
+
+      warningNotification.appendChild(iconContainer);
+      warningNotification.appendChild(warningContainer);
+
+      const hasHeroField = document.querySelector('.js-form-item-field-has-hero-value');
+      console.log(heroCheckbox.checked);
+      if (hasHeroField && heroCheckbox.checked) {
+        hasHeroField.insertAdjacentElement('afterend', warningNotification);
+      }
+
+      if (heroCheckbox) {
+        heroCheckbox.addEventListener('click', function () {
+          if (this.checked) {
+            let addHero = document.querySelector(
+              '[data-hdbt-selector="field-hero-action--add"]'
+            );
+            if (addHero) {
+              triggerEvent(addHero, 'mousedown');
+              hasHeroField.insertAdjacentElement('afterend', warningNotification);
+            }
+          } else if (!this.checked) {
+            let removeHero = document.querySelector(
+              '[data-hdbt-selector="field-hero-action--remove"]'
+            );
+            if (removeHero) {
+              triggerEvent(removeHero, 'mousedown');
+              if (isBasicPage && document.querySelector('.edit-form-notification--warning')) {
+                document.querySelector('.edit-form-notification--warning').remove();
+              }
+            }
+          }
+        });
       }
     }
-  });
-}
+  };
+})(Drupal);
